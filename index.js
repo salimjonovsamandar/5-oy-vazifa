3eb4186f214f5db526962c5854027078b772c5a8
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2023-12-25 05:58:48 +0500

    5 oy 2 vazifa bajarildi

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 // 1-masala
-let n = prompt("Ixtiyoriy sonlarni kiriting")
-function sum(arg){
-    let sum = 0;
-    for (const item of arg) {
-        if(item%2==0){
-            sum += item**2
-        }
-    }
-    return sum
-}
-console.log(sum(n));
+// function numbereven(arg) {
+//   let sum = 0;
+//   for (const item of arg) {
+//     if (typeof item != "number") {
+//       throw new Error("Massiv ichidagi element bunday bo'lishi mumkin emas");
+//     }
+//     if (item % 2 == 0) {
+//       sum += item ** 2;
+//     }
+//   }
+//   return sum;
+// }
+// try {
+//   console.log(numbereven([1, 2, 3, 4, 5, 6, 7, 8]));
+// } catch (error) {
+//   console.log(error);
+// }
 
-
-// 2-masala
-function words(arg){
-    let newArr = []
-    for (const item of arg) {
-        newArr.push(item[0])
-    }
-    return newArr
-}
-// console.log(words(["salom", "dunyo"]));
-
+// // 2-masala
+// function words(arg) {
+//   let newArr = [];
+//   for (const item of arg) {
+//     if (typeof item != "string") {
+//       throw new Error("Massiv ichidagi element bunday bo'lishi mumkin emas");
+//     } else {
+//       newArr.push(item[0]);
+//     }
+//   }
+//   return newArr;
+// }
+// try {
+//   console.log(words([1, "salom", "dunyo"]));
+// } catch (error) {
+//   console.log(error);
+// }
 
 // 3-masala
 // let n = {
@@ -29,59 +41,57 @@ function words(arg){
 //     name: "John",
 //     born: 2003,
 // }
-function allSum(arg){
-    let a = Object.values(arg)
-    let s = 0
-    for (const item of a) {
-        if(typeof item == "number"){
-            s += item
-        }
-    }
-    return s
-}
+// function allSum(arg) {
+//   let a = Object.values(arg);
+//   let s = 0;
+//   for (const item of a) {
+//     if (typeof item == "number") {
+//       s += item;
+//     }
+//   }
+//   return s;
+// }
 // console.log(allSum(n));
 
+// // 4-masala
+// // let n = [1, 2, 3, 4, 5, 6, 7]
+// function tub(arg) {
+//   let counter = 0;
+//   for (let i = 1; i <= arg; i++) {
+//     if (arg % i == 0) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+// function sumTub(arg) {
+//   let res = 0;
+//   for (const item of arg) {
+//     if (tub(item) == 2) {
+//       res += item;
+//     }
+//   }
+//   return res;
+// }
+// // console.log(sumTub(n));
 
-// 4-masala
-// let n = [1, 2, 3, 4, 5, 6, 7]
-function tub(arg){
-    let counter = 0;
-    for(let i = 1; i<=arg; i++){
-        if(arg%i==0){
-            counter++
-        }
-    }
-    return counter
-}
-function sumTub(arg){
-    let res = 0;
-    for (const item of arg) {
-        if(tub(item)==2){
-            res += item
-        }
-    }
-    return res
-}
-// console.log(sumTub(n));
-
-
-// 5-masala
-// let n = 123457;
-function odd(arg){
-    let counter = 0;
-    for(let i =1; i<=arg; i*=10){
-        counter++
-    }
-    return counter
-}
-function oddSum(arg){
-    let sum = 0;
-    for(let i = odd(arg); i>=0; i--){
-        let a = Math.trunc(arg/(10**(i-1)))%10
-        if(a%2==1){
-            sum += Math.trunc(arg/(10**(i-1)))%10
-        }
-    }
-    return sum
-}
-// console.log(oddSum(n));
+// // 5-masala
+// // let n = 123457;
+// function odd(arg) {
+//   let counter = 0;
+//   for (let i = 1; i <= arg; i *= 10) {
+//     counter++;
+//   }
+//   return counter;
+// }
+// function oddSum(arg) {
+//   let sum = 0;
+//   for (let i = odd(arg); i >= 0; i--) {
+//     let a = Math.trunc(arg / 10 ** (i - 1)) % 10;
+//     if (a % 2 == 1) {
+//       sum += Math.trunc(arg / 10 ** (i - 1)) % 10;
+//     }
+//   }
+//   return sum;
+// }
+// // console.log(oddSum(n));
