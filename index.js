019e1c96e8bc76e4e99f25d30a15313c38d8c876
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-01-02 11:47:57 +0500

    4 vazifa bajarildi

--- index.js
+++ index.js
@@ -1,58 +1,136 @@
-const data = [
-  {
-    id: 1,
-    name: "Sity",
-    img: "https://picsum.photos/id/238/200/300",
-    description: "Here is a picture of the city",
-  },
+const btn = document.getElementById("btn");
+const name = document.getElementById("name");
+const age = document.getElementById("age");
+const car = document.getElementById("car");
+const desc = document.getElementById("description");
+const tbody = document.getElementById("tbody");
 
-  {
-    id: 2,
-    name: "Railway",
-    img: "https://picsum.photos/id/1/200/300",
-    description: "Here is a picture of the railway",
-  },
+function validate() {
+  if (!name.value) {
+    name.focus();
+    name.style.outlineColor = "red";
+    return false;
+  }
+  //   else {
+  //     name.style.OutlineColor = "lightgray";
+  //   }
 
-  {
-    id: 3,
-    name: "Road",
-    img: "https://picsum.photos/id/17/200/300",
-    description: "Here is a picture of the road",
-  },
+  if (!age.value) {
+    age.focus();
+    age.style.outlineColor = "red";
+    return false;
+  }
 
-  {
-    id: 4,
-    name: "Book",
-    img: "https://picsum.photos/id/24/200/300",
-    description: "Here is a picture of the book",
-  },
+  if (!age.value) {
+    age.focus();
+    age.style.outlineColor = "red";
+    return false;
+  }
+  if (age.value <= 0 || age.value >= 150) {
+    alert("Bunday yosh kiritish mumkin emas");
+    age.focus();
+    return false;
+  }
 
-  {
-    id: 5,
-    name: "Mountain",
-    img: "https://picsum.photos/id/29/200/300",
-    description: "Here is a picture of snowy mountains",
-  },
+  if (!Number(age.value)) {
+    alert("Yosh raqamda kiritilishi kerak");
+    age.focus();
+    return false;
+  } else {
+    name.style.outlineColor = "lightgray";
+  }
+  if (!car.value) {
+    age.focus();
+    alert("Mashinani tanlang");
+    return false;
+  }
+  if (!desc.value) {
+    age.focus();
+    alert("Izoh yozish majburiy");
+    return false;
+  } else {
+    name.style.outlineColor = "lightgray";
+  }
 
-  {
-    id: 6,
-    name: "Nature",
-    img: "https://picsum.photos/id/28/200/300",
-    description: "Here is a picture of nature",
-  },
-];
+  return true;
+}
+function clear() {
+  name.value = "";
+  age.value = "";
+  car.value = "";
+  desc.value = "";
+}
 
-const cardImg = document.querySelector("#img");
-const cardTitle = document.getElementById("title");
-const cardDescription = document.getElementById("comment");
-const btn = document.querySelector("#btn");
+btn &&
+  btn.addEventListener("click", function () {
+    if (validate()) {
+      const user = {
+        id: Date.now(),
+        name: name.value,
+        age: age.value,
+        car: car.value,
+        desc: desc.value,
+      };
 
-btn.addEventListener("click", function () {
-  setTimeout(function () {
-    const card = data[Math.trunc(Math.floor(Math.random(0, 1) * data.length))];
-    cardImg.setAttribute("src", card["img"]);
-    cardTitle.innerHTML = card.name;
-    cardDescription.innerHTML = card.description;
-  }, 500);
+      let dataLocalStorage = [];
+      if (localStorage.getItem("users")) {
+        dataLocalStorage = JSON.parse(localStorage.getItem("users"));
+      }
+      dataLocalStorage.push(user);
+      localStorage.setItem("users", JSON.stringify(dataLocalStorage));
+
+      const tr = createRow(user, dataLocalStorage.length);
+      tbody.innerHTML += tr;
+      clear();
+    }
+  });
+
+function createRow(user, index) {
+  return `
+        <tr>
+          <td>${index}</td>
+          <td>${user.name}</td>
+          <td>${user.age}</td>
+          <td>${user.car}</td>
+          <td>${user.desc}</td>
+          <td>
+          <span class="update">Update</span>
+          <span data-id = 'item_${user.id}' class="delete">Delete</span>
+          </td>
+        </tr>
+  `;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let data = [];
+  if (localStorage.getItem("users")) {
+    data = JSON.parse(localStorage.getItem("users"));
+  }
+  if (data.length && tbody) {
+    data.forEach((element, index) => {
+      const tr = createRow(element, index + 1);
+      tbody.innerHTML += tr;
+    });
+  }
+
+  const deleteButtons = document.querySelectorAll("span.delete");
+
+  if (deleteButtons.length) {
+    deleteButtons.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        const elId = this.getAttribute("data-id").slice(5);
+        console.log(elId);
+        if (elId) {
+          let isDelete = confirm("Rosdan ham o'chirmoqchimisiz");
+          if (isDelete) {
+            data = data.filter((el) => {
+              return el.id != elId;
+            });
+localStorage.setItem("users", JSON.stringify(data))
+window.location.reload();
+          }
+        }
+      });
+    });
+  }
 });
-console.log(card);
